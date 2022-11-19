c3e3a0b5773010da7f163532c48f25e0f5264ce4
sadegh <sbarotcob@gmail.com>
2022-11-19 17:10:05 +0330

    bugfix

--- src/routes.php
+++ src/routes.php
@@ -1,19 +1,21 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'Colbeh\Consts\Controllers', 'prefix' => 'builder'], function () {
+if(config('app.debug')) {
+
+	Route::group(['namespace' => 'Colbeh\Consts\Controllers', 'prefix' => 'builder'], function () {
 //	Route::get('/', ['as' => 'bmi_path', 'uses' => 'ConstController@index']);
 
-	Route::get('/', "ConstController@index");
-	Route::get('/const/table', "ConstController@index");
-	Route::get('/const/column', "ConstController@column");
-	Route::post('/const/add', "ConstController@add");
-	Route::post('/const/column', "ConstController@columnAdd");
+		Route::get('/', "ConstController@index");
+		Route::get('/const/table', "ConstController@index");
+		Route::get('/const/column', "ConstController@column");
+		Route::post('/const/add', "ConstController@add");
+		Route::post('/const/column', "ConstController@columnAdd");
 
-    Route::get('/vue', "VueBuilderController@index");
-    Route::post('/vue/create', "VueBuilderController@vueCreate");
+		Route::get('/vue', "VueBuilderController@index");
+		Route::post('/vue/create', "VueBuilderController@vueCreate");
 
-});
+	});
 
-
+}
 
